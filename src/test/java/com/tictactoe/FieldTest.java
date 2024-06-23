19cde4bbfe263b2e92bcac9c92759edbf0279213
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-06-23 19:40:57 +0200

    add Field tests & add dependency

---
+++ src/test/java/com/tictactoe/FieldTest.java
@@ -0,0 +1,96 @@
+package com.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class FieldTest {
+
+    @Test
+    void allFieldCellsShouldBeEmptyWhenFieldCreated() {
+        Map<Integer, Sign> field = new Field().getField();
+        field.forEach((integer, sign) -> assertSame(sign, Sign.EMPTY));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    void shouldReturnRightEmptyFieldIndex(int fieldIndex) {
+        Field field = new Field();
+        field.getField().forEach((integer, sign) -> {
+            if (integer != fieldIndex) {
+                field.getField().put(integer, Sign.CROSS);
+            }
+        });
+
+        assertEquals(fieldIndex, field.getEmptyFieldIndex());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    void getFieldData(int fieldIndex) {
+        Field field = new Field();
+        field.getField().forEach((integer, sign) -> {
+            if (integer != fieldIndex) {
+                field.getField().put(integer, integer % 2 == 0 ? Sign.NOUGHT : Sign.CROSS);
+            }
+        });
+
+        Map<Integer, Sign> expectedField = new TreeMap<>();
+        for (int integer = 0; integer < 9; integer++) {
+            if (integer != fieldIndex) {
+                expectedField.put(integer, integer % 2 == 0 ? Sign.NOUGHT : Sign.CROSS);
+            } else {
+                expectedField.put(integer, Sign.EMPTY);
+            }
+        }
+
+        assertEquals(expectedField, field.getField());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 2",
+            "3, 4, 5",
+            "6, 7, 8",
+            "0, 3, 6",
+            "1, 4, 7",
+            "2, 5, 8",
+            "0, 4, 8",
+            "2, 4, 6"
+    })
+    void shouldReturnCROSSSignWhenCalledCheckWin(int firstFieldIndex, int secondFieldIndex, int thirdFieldIndex) {
+        Field field = new Field();
+        field.getField().put(firstFieldIndex, Sign.CROSS);
+        field.getField().put(secondFieldIndex, Sign.CROSS);
+        field.getField().put(thirdFieldIndex, Sign.CROSS);
+
+        assertSame(Sign.CROSS, field.checkWin());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 2",
+            "3, 4, 5",
+            "6, 7, 8",
+            "0, 3, 6",
+            "1, 4, 7",
+            "2, 5, 8",
+            "0, 4, 8",
+            "2, 4, 6"
+    })
+    void shouldReturnNOUGHTSignWhenCalledCheckWin(int firstFieldIndex, int secondFieldIndex, int thirdFieldIndex) {
+        Field field = new Field();
+        field.getField().put(firstFieldIndex, Sign.NOUGHT);
+        field.getField().put(secondFieldIndex, Sign.NOUGHT);
+        field.getField().put(thirdFieldIndex, Sign.NOUGHT);
+
+        assertSame(Sign.NOUGHT, field.checkWin());
+    }
+}

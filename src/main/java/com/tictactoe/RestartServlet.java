c76e4574b8b6c4ed9be8296bad7ae7b0d88b8644
Roman Yehorov <roman.yehorov@student.tuke.sk>
2024-06-25 21:59:18 +0200

    add logs for RestartServlet

--- src/main/java/com/tictactoe/RestartServlet.java
+++ src/main/java/com/tictactoe/RestartServlet.java
@@ -1,5 +1,8 @@
 package com.tictactoe;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +11,12 @@ import java.io.IOException;
 
 @WebServlet(name = "RestartServlet", value = "/restart")
 public class RestartServlet extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getLogger(RestartServlet.class);
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         req.getSession().invalidate();
         resp.sendRedirect("/start");
+        LOGGER.info("Game restarted");
     }
 }
